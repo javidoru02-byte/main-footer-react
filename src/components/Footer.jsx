c58385874f67d2b937b64ad24ecd18adeaa9da81
David <javidoru02@gmail.com>
2026-06-26 17:25:03 +0300

    Initial commit

---
+++ src/components/Footer.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+function Footer() {
+  return (
+    <footer className="footer">
+      <h3>контакти</h3>
+      <p>Адреса: Протасевича 123</p>
+      <p>Електронна пошта: info@example.com</p>
+      <p>Телефон: +1 (123) 456-7890</p>
+    </footer>
+  );
+}
+
+export default Footer;
---
+++ src/components/Header.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import Main from "./Main";
+import Footer from "./Footer";
+
+function Header() {
+  return (
+    <div className="page">
+      <header className="header">
+        <h1>Hdrezka</h1>
+      </header>
+      <Main />
+      <Footer />
+    </div>
+  );
+}
+export default Header;
